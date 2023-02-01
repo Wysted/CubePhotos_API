09f61f71ffe60120469eb6fc04bf7b2f108dc592
Wysted <vladimirbesser@gmail.com>
2023-02-01 03:41:09 -0300

    Cambio de puerto

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.delete('/api/cubephoto/:id_cubephoto', async (request, response) => {
     }
 }); 
 
-const PORT = 3001
+const PORT = 80
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
